3de4afa594451199f5d906f9d1dfbde4e50cdd1a
milleny <125597270+millenyleop@users.noreply.github.com>
2023-11-30 10:51:48 -0300

    Add files via upload

---
+++ telapaciente.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta Agendada</title>
+    <style>
+        body {
+            background-color: #f0f8ff; /* Um tom claro de azul (alice blue) */
+            text-align: center;
+            padding: 50px;
+            font-family: Arial, sans-serif;
+        }
+
+        h1 {
+            color: #000080; /* Azul escuro */
+        }
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 18px;
+            background-color: #27274B;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-decoration: none;
+            margin: 10px;
+        }
+        .button:hover {
+            background-color: #27274B;
+        }
+    </style>
+</head>
+<body>
+    <h1>Parabéns, você agendou sua consulta com sucesso!</h1>
+    <a href="painel.php" class="button" >Sair</a>
+</body>
+</html>
